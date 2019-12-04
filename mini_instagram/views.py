2f840cb7e9044e3508826b4e1de523aeea4a7349
Castiel <castielwongsysu@gmail.com>
2019-12-04 14:20:52 +1030

    Add login function

--- mini_instagram/views.py
+++ mini_instagram/views.py
@@ -1,5 +1,13 @@
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+
+from django.contrib.auth.forms import UserCreationForm
 
 class HelloDjango(TemplateView):
     template_name = 'index.html'
-    
+
+class AuthLogin(CreateView):
+    form_class = UserCreationForm
+    template = 'registration/login.html'
+    success_url = reverse_lazy('posts')
