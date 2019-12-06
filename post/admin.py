4d4b8f4e9b575fe416347940e2fea52ec6f7ef7d
Castiel <castielwongsysu@gmail.com>
2019-12-06 17:30:50 +1030

    Add feature Likes

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from post.models import Post, CustomUser
+from post.models import Post, CustomUser, Like
 
 # register the model in admin page
 admin.site.register(CustomUser)
 admin.site.register(Post)
+admin.site.register(Like)
--- post/models.py
+++ post/models.py
@@ -13,17 +13,45 @@ class CustomUser(AbstractUser):
     )
 
 class Post(models.Model):
+    author = models.ForeignKey(
+        CustomUser,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='instagram_posts',
+    )
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to='static/images/posts',
         format='JPEG',
         options={'quality': 100},
-        null=True,
         blank=True,
+        null=True,
     )
+    posted_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.title
 
     # return to its detail page when it's created
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
-    
 
+    def get_like_count(self):
+        return self.likes.count()
+    
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("post", "user")
+    
+    def __str__(self):
+        return f"Like: {self.user.username} likes {self.post.title}"
+    
