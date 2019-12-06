e01fad23adb605f323700da06e4503f0070a1bbc
Castiel <castielwongsysu@gmail.com>
2019-12-06 22:21:25 +1030

    Add feature Comments and put Like and Comment inline with Post

--- post/admin.py
+++ post/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
-from post.models import Post, CustomUser, Like
+from post.models import Post, CustomUser, Like, Comment
+
+# make models inline
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+class LikeInline(admin.StackedInline):
+    model = Like
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+        LikeInline,
+    ]
 
 # register the model in admin page
 admin.site.register(CustomUser)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Like)
+admin.site.register(Comment)
--- post/models.py
+++ post/models.py
@@ -45,13 +45,25 @@ class Post(models.Model):
     def get_like_count(self):
         return self.likes.count()
     
+    def get_comment_count(self):
+        return self.comments.count()
+    
 class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ("post", "user")
     
     def __str__(self):
-        return f"Like: {self.user.username} likes {self.post.title}"
+        return f"{self.user.username} likes {self.post.title}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
+    posted_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.comment
     
