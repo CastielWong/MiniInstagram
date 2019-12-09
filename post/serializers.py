5b67a18e93863b7cfff0740bb177cfea3b8e667a
Castiel <castielwongsysu@gmail.com>
2019-12-09 22:01:44 +1030

    Fix all serializer to be read-only and append pk

--- post/serializers.py
+++ post/serializers.py
@@ -2,17 +2,20 @@ from rest_framework import serializers
 
 from post.models import Post, Like, Comment
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['author', 'title', 'posted_on', 'likes']
+        fields = ['pk', 'author', 'title', 'posted_on', 'likes', 'comments']
+        read_only_fields = ['author', 'title', 'posted_on', 'likes']
 
-class LikeSerializer(serializers.HyperlinkedModelSerializer):
+class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
-        fields = ['post', 'user']
+        fields = ['pk', 'post', 'user']
+        read_only_fields = ['post', 'user']
 
-class CommentSerializer(serializers.HyperlinkedModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['post', 'user', 'comment', 'posted_on']
+        fields = ['pk', 'post', 'user', 'comment', 'posted_on']
+        read_only_fields = ['post', 'user', 'comment', 'posted_on']
--- user/serializers.py
+++ user/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 
 from user.models import CustomUser, UserConnection
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['user_alias','email', 'url']
+        fields = ['pk', 'user_alias','email', 'url']
+        read_only_fields = ['user_alias','email', 'url']
 
-class UserConnectionSerializer(serializers.HyperlinkedModelSerializer):
+class UserConnectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserConnection
-        fields =['follower', 'following']
+        fields = ['pk', 'follower', 'following']
+        read_only_fields = ['follower', 'following']
 
